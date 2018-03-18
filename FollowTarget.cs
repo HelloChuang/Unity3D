c3cb3ddc24054dea271c3eda40736d3f810954c8
HelloChuang <Gaochuang_@outlook.com>
2018-03-18 21:35:01 +0800

    Add files via upload

---
+++ FollowTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+    public Vector3 offset;
+    private Transform player;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void Update() {
+        transform.position = player.position + offset;
+    }
+}
